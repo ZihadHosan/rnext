1c413b5d9529beb397204c7c0a1dc2f1265962f8
Zihad Hosan <zihadhossain88@hotmail.com>
2024-04-04 12:09:59 +0600

    tasker update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
+import ArrayMethodComp from "./components/ArrayMethod";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
+import BatchUpdate from "./components/Practice";
 
 function App() {
   return (
     <>
       <Header />
       <HeroSection />
+      <BatchUpdate />
+      <ArrayMethodComp />
       <Footer />
     </>
   );
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -5,7 +5,7 @@ export default function HeroSection() {
         <>
             {/* Begin hero */}
             <section className="pb-[114px] pt-20 md:mt-[100px]">
-                <div className="container lg:px-20">
+                <div className="container lg:px-20lg:px-20">
                     <div className="grid items-center gap-6 md:grid-cols-2">
                         <div className="flex justify-center md:order-2">
                             <img
