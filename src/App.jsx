30c44497080c725cb402f652a35c502ad0bb3f1c
Zihad Hosan <zihadhossain88@hotmail.com>
2024-04-16 00:39:28 +0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,9 @@ import NewForm from "./components/NewForm";
 import "./styles.css";
 
 function App() {
-  const statuses = ["empty", "typing", "submitting", "success", "error"];
-  //console.log(statuses);
   return (
     <>
-      {statuses.map((status) => (
-        <NewForm key={status} status={status} />
-      ))}
+      <NewForm />
     </>
   );
 }
--- src/components/NewForm.jsx
+++ src/components/NewForm.jsx
@@ -1,26 +1,63 @@
 /* eslint-disable react/prop-types */
-export default function NewForm({ status = "empty" }) {
-  if (status === "success")
-    return (
-      <>
-        <h1>Thats right!</h1> <hr />
-      </>
-    );
+import { useState } from "react";
+
+function submitForm(answer) {
+  // Pretend it's hitting the network.
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (answer.toLowerCase() === "barishal") {
+        resolve();
+      } else {
+        reject(new Error("Good guess but a wrong answer. Try again!"));
+      }
+    }, 3000);
+  });
+}
+
+export default function NewForm() {
+  const [answer, setAnswer] = useState("");
+  const [error, setError] = useState(null);
+  const [status, setStatus] = useState("typing");
+  //console.log(status);
+
+  //   Handles
+
+  const handelTextChange = (e) => {
+    setAnswer(e.target.value);
+    setError(null);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setStatus("submitting");
+    try {
+      await submitForm(answer);
+      setStatus("success");
+    } catch (err) {
+      setStatus("typing");
+      setError(err.message);
+    }
+  };
+
+  if (status === "success") return <h1>Thats right!</h1>;
   return (
     <>
       <h2>City quiz</h2>
       <p>What city is located on two continents?</p>
 
-      <form>
-        <textarea disabled={status === "submitting"}></textarea>
+      <form onSubmit={handleSubmit}>
+        <textarea
+          value={answer}
+          onChange={handelTextChange}
+          disabled={status === "submitting"}
+        ></textarea>
         <br />
-        <button disabled={status === "submitting" || status === "empty"}>
+        <button disabled={status === "submitting" || answer === ""}>
           Submit
         </button>
         {status === "submitting" && <p>Loading...</p>}
-        {status === "error" && <p className="Error">There was an error</p>}
+        {error && <p className="Error">{error}</p>}
       </form>
-      <hr />
     </>
   );
 }
